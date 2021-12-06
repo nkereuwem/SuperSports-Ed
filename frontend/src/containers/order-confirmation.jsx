d978b0d926b3c9ef1ede154583f15eb500adc526
nkereuwem <edet.nkereuwem@yahoo.com>
2021-12-05 21:58:01 -0600

    Changed

--- frontend/src/containers/order-confirmation.jsx
+++ frontend/src/containers/order-confirmation.jsx
@@ -1,9 +1,11 @@
 import React, { useEffect } from "react";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch, connect } from "react-redux";
 import { getUser } from "../reducks/users/selectors";
+import { push } from "connected-react-router";
 
 const OrderConfirmation = () => {
   const selector = useSelector((state) => state);
+  const dispatch = useDispatch();
   const user = JSON.parse(localStorage.getItem("LOGIN_USER_KEY"));
   useEffect(() => {
     console.log(user);
@@ -25,9 +27,7 @@ const OrderConfirmation = () => {
         </div>
         <div class="details">
           <form action="">
-            <button>
-              <a href="/">Back to Home</a>
-            </button>
+            <button onClick={() => dispatch(push("/"))}>Back to Home</button>
           </form>
         </div>
       </section>
